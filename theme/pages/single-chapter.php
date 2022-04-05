e3b11c0bb0c13849a47798a7f8e8d1182394ed53
Manav Yadav <manav.y1998@gmail.com>
2022-04-05 14:12:26 +0530

    Message listing.

---
+++ theme/pages/single-chapter.php
@@ -0,0 +1,48 @@
+<?php include_once __DIR__ . '/_header.php'?>
+<pre>
+<?php
+$heads = $functions->reph1($postdata['title']);
+$messages = array();
+foreach($postdata['messages'] as $i=>$m){
+    $messages[$i] = $functions->reph1($m);
+}
+?>
+</pre>
+<section class="container d-flex">
+
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <?php
+                foreach($heads as $head):
+                ?>
+                <th scope="col"><?=$head[0]?></th>
+                <?php
+                endforeach;
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach($messages as $i=>$message):
+            ?>
+            <tr>
+                <th scope="row"><?=$i+1?></th>
+                <?php
+                foreach($message as $m):
+                ?>
+                <td><?=$m[0]?></td>
+                <?php
+                endforeach;
+                ?>
+                
+            </tr>
+            <?php
+            endforeach;
+            ?>
+            
+        </tbody>
+    </table>
+</section>
+<?php include_once __DIR__ . '/_footer.php'?>
