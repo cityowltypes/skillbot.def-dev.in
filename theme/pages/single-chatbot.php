f27e6a8b342839995d49176b3148e307402aec74
akshay / jigmet <akshay.wildfire@pm.me>
2022-04-15 17:20:21 +0530

    starting point for get_identifier_of_last_message_sent

--- theme/pages/single-chatbot.php
+++ theme/pages/single-chatbot.php
@@ -4,9 +4,15 @@ include_once __DIR__ . '/../_init.php';
 //SEE IF ANY RESPONSE HAS BEEN RECEIVED FROM TELEGRAM
 $telegram_response = (array) $api->body()['message'];
 
+//GET KEY OF LAST MESSAGE SENT
+$key_of_last_message_sent = $functions->get_last_message_sent($telegram_user_id, $slug);
+
+//$dash->getAttribute( $dash->get_ids ( array( 'telegram_user_id'=>$telegram_user_id, 'chatbot'=>$slug ), 'AND', '=' )[0], 'identifier_of_last_message_sent' );
+
 if ($telegram_response) {
 	//LOG ALL RESPONSES
 	$functions->log_response($telegram_response, $slug, 'telegram');
+	$functions->save_response($telegram_response, $slug);
 }
 
 //IF THIS IS THE FIRST MESSAGE OR IF THE USER WANTS TO SWITCH LANGUAGE
