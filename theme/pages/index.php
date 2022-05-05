39d24685375073374db148aa3dc5ea91922aa3d9
akshay.wildfire / tw <akshay.wildfire@pm.me>
2022-05-06 04:12:46 +0530

    home page - list of all chatbots

--- theme/pages/index.php
+++ theme/pages/index.php
@@ -1,4 +1,10 @@
 <?php include_once __DIR__ . '/_header.php'?>
 
+<div class="container py-5">
+	<a href="/admin" class="w-100 d-block btn btn-warning my-2 btn-lg">Admin Panel</a>
+<?php foreach ($dash->getObjects($dash->get_ids(array('type'=>'chatbot'), '=', 'AND')) as $chatbot) { ?>
+	<a href="https://t.me/<?= $chatbot['chatbot_handle'] ?>" target="new" class="w-100 d-block btn btn-primary my-2 btn-lg"><?= $chatbot['title'] ?></a>
+<?php } ?>
+</div>
 
 <?php include_once __DIR__ . '/_footer.php'?>
