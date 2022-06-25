445995fc4928246d730871738b4936da7428300b
akshay / jigmet <cityowl@hey.com>
2022-06-25 17:43:38 +0530

    response print

---
+++ theme/pages/single-response.php
@@ -0,0 +1,7 @@
+<?php include_once __DIR__ . '/_header.php'?>
+
+<pre><?php print_r($postdata) ?></pre>
+
+<?php //print_r($functions->derephrase($str, 1, [], 1)) ?>
+
+<?php include_once __DIR__ . '/_footer.php'?>
