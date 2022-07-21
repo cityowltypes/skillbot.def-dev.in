e6e1ab381749f093e386472371ae05c3297e55ab
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-07-21 19:40:55 +0530

    interface for chatbot selection screen

--- theme/pages/report/includes/_nav.php
+++ theme/pages/report/includes/_nav.php
@@ -1,5 +1,16 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
     <div class="container-fluid justify-content-start">
+        <?php
+        if (isset($_GET['handle'])):
+        ?>
         <button id="toggle" class="hamburger ms-lg-2 px-0" data-expanded="false"><span></span></button>
         <a class="navbar-brand me-0 ms-3" href="<?= "/report/chatbot?id={$_GET['id']}&handle={$_GET['handle']}" ?>">@<?=$_GET['handle']?></a>
+        <?php
+        else:
+        ?>
+        <a class="navbar-brand me-0 ms-3" href="<?= "/report" ?>"><i class="fal fa-analytics"></i>&nbsp;Insights Dashboard</a>
+        <?php
+        endif;
+        ?>
+        <a href="/admin" class="btn btn-warning ms-auto">Admin Panel</a>
 </nav>
--- theme/pages/report/index.php
+++ theme/pages/report/index.php
@@ -1,11 +1,21 @@
-<?php include_once __DIR__ . '/../_header.php'?>
+<?php
+include_once __DIR__ . '/../_header.php';
+require_once 'includes/_nav.php';
+?>
 
-<div class="container py-5">
-	<h1 class="display-4 text-center mb-3"><span class="fal fa-analytics"></span>&nbsp;Insights Dashboard</h1>
-<?php foreach ($dash->getObjects($dash->get_ids(array('type'=>'chatbot'), '=', 'AND')) as $chatbot) { ?>
-	<a href="/report/chatbot?id=<?= $chatbot['id'] ?>&handle=<?= $chatbot['chatbot_handle'] ?>" target="new" class="w-100 d-block btn btn-primary my-2 btn-lg"><?= $chatbot['title'] ?></a>
-<?php } ?>
-	<a href="/admin" class="w-100 d-block btn btn-warning my-2 btn-lg">Admin Panel</a>
+<div class="main">
+    <div id="dash-viewport" class="container py-5">
+        <p class="display-2 border-bottom mb-3">Chatbots</p>
+        <?php foreach ($dash->getObjects($dash->get_ids(array('type'=>'chatbot'), '=', 'AND')) as $chatbot) : ?>
+        <a
+            href="/report/chatbot?id=<?= $chatbot['id'] ?>&handle=<?= $chatbot['chatbot_handle'] ?>"
+            target="_blank"
+            class="btn btn-primary my-2 btn-lg">
+            <?= $chatbot['title'] ?>
+        </a>
+        <?php endforeach ?>
+
+    </div>
 </div>
 
 <?php include_once __DIR__ . '/../_footer.php'?>
