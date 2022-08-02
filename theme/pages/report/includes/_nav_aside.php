9f580cb110099dcfefd4f9bfbb0c15b1356baeef
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-08-02 15:13:35 +0530

    removed pre and post assessment download options

--- theme/pages/report/includes/_nav_aside.php
+++ theme/pages/report/includes/_nav_aside.php
@@ -18,21 +18,7 @@
             <a
                 href="/report/user-data?id=<?= $_GET['id'] ?>&handle=<?=$_GET['handle']?>"
                 class="w-100 btn text-white rounded-0 text-start text-decoration-none">
-                <i class="fad fa-download me-2"></i>User Data
-            </a>
-        </li>
-        <li class="nav-item">
-            <a
-                href="/report/pre-assessment-data?id=<?= $_GET['id'] ?>&handle=<?=$_GET['handle']?>"
-                class="w-100 btn text-white rounded-0 text-start text-decoration-none">
-                <i class="fad fa-download me-2"></i>Pre-assessment Data
-            </a>
-        </li>
-        <li class="nav-item">
-            <a
-                href="/report/post-assessment-data?id=<?= $_GET['id'] ?>&handle=<?=$_GET['handle']?>"
-                class="w-100 btn text-white rounded-0 text-start text-decoration-none">
-                <i class="fad fa-download me-2"></i>Post-assessment Data
+                <i class="fad fa-download me-2"></i>Download
             </a>
         </li>
     </ul>
