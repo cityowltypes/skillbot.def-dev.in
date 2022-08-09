b5bbd28b2ba2ad8ad8f3fce1ba6148c9a7fba01f
Manav Yadav <manav.y1998@gmail.com>
2022-08-09 14:52:46 +0530

    Removed dd

--- theme/pages/report/export.php
+++ theme/pages/report/export.php
@@ -56,7 +56,7 @@ foreach ($data as $row) {
 	$export[$i]['response_id'] = $row['id'];
 	$export[$i]['created_on'] = date('d, M Y H:i', $row['created_on']);
 
-	dd($row);
+	//dd($row);
 
 	for ($j=1; $j < 10 ; $j++) {
         $export[$i]["id__{$user_form_id}__{$j}"] = $row["id__{$user_form_id}__{$j}"];
