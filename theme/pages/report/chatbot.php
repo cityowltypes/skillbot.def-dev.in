19cfeb9738f4d57a04eb003a9fe9aa8cd18de316
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-08-17 16:41:07 +0530

    removed 100% width from reset & reload

--- theme/pages/report/chatbot.php
+++ theme/pages/report/chatbot.php
@@ -81,7 +81,7 @@ echo "<style>
     <div id="dash-viewport" class="pb-5 pt-2">
         <a
                 href="<?= "/report/chatbot?id={$_GET['id']}&handle={$_GET['handle']}" ?>"
-                class="w-100 btn rounded-0 text-start text-decoration-none">
+                class="btn rounded-0 text-start text-decoration-none">
             <i class="fad fa-redo me-2"></i>Reset & Reload
         </a>
         <?php if (trim($bot['logos_footer'] ?? '')): ?>
