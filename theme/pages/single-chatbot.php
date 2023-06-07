03c40c6aa9fb50790beef13ed5aa6d898fc6ebd9
cityowl / jigmet <cityowl@hey.com>
2023-06-07 15:57:04 +0530

    type safety

--- theme/pages/single-chatbot.php
+++ theme/pages/single-chatbot.php
@@ -25,7 +25,7 @@ if ($telegram_user_id = $telegram_response['from']['id'] ?? false) {
 	$response_id = $sql->executeSQL("SELECT `id` FROM `data` WHERE `content_privacy`='private' AND `content`->'$.telegram_user_id' = ".$telegram_user_id." AND `content`->'$.chatbot' = '".$chatbot_slug."' AND `content`->'$.type' = 'response'")[0]['id'];
 	$main_response_id = $response_id;
 
-	$current_user = ($dash->getAttribute($main_response_id , 'current_user') ?? '0');
+	$current_user = ($dash->getAttribute((int) $main_response_id , 'current_user') ?? '0');
 	if (((int) $current_user) >= 2) {
 		$telegram_user_id = $telegram_user_id.'-'.$current_user;
 		$response_id = $sql->executeSQL("SELECT `id` FROM `data` WHERE `content_privacy`='private' AND `content`->'$.telegram_user_id' = '".$telegram_user_id."' AND `content`->'$.chatbot' = '".$chatbot_slug."' AND `content`->'$.type' = 'response'")[0]['id'];
@@ -46,13 +46,13 @@ if ($telegram_user_id = $telegram_response['from']['id'] ?? false) {
 
 	else if ($response_id) {
 		//GET LAST SENT MESSAGE
-		$last_message_identifier = $dash->getAttribute($response_id , 'last_message_identifier');
+		$last_message_identifier = $dash->getAttribute((int) $response_id , 'last_message_identifier');
 		
 		//NEXT MESSAGE IDENTIFIER
 		if ($last_message_identifier == 'lang##'.$chatbot_id) {
 			$dash->pushAttribute($response_id, 'lang', $telegram_response['text']);
 			//GET USER LANGUAGE
-			$telegram_user_lang = $dash->getAttribute($response_id , 'lang');
+			$telegram_user_lang = $dash->getAttribute((int) $response_id , 'lang');
 			$next_message_identifier = 'id##'.$chatbot_id;
 		}
 		else if ($last_message_identifier == 'cert##'.$chatbot_id) {
@@ -66,15 +66,15 @@ if ($telegram_user_id = $telegram_response['from']['id'] ?? false) {
 		}
 		else {
 			//GET USER LANGUAGE
-			$telegram_user_lang = $dash->getAttribute($response_id , 'lang');
-			$last_message_response_options = json_decode($dash->getAttribute($response_id, 'last_message_response_options'), true) ?? [];
+			$telegram_user_lang = $dash->getAttribute((int) $response_id , 'lang');
+			$last_message_response_options = json_decode($dash->getAttribute((int) $response_id, 'last_message_response_options'), true) ?? [];
 			$next_message_identifier = array_search($telegram_response['text'], ($last_message_response_options ?? []));
         	
         	if (substr($next_message_identifier, 0, 12) == 'switchuser##') {
 				$dash->pushAttribute($main_response_id, implode('__', $functions->derephrase($next_message_identifier)), $telegram_response['text']);
 	        	$current_user = $functions->derephrase($next_message_identifier)[2];
 	        	if ($current_user == 'new') {
-	                $multiuser_count = $dash->getAttribute($main_response_id, 'multiuser_count');
+	                $multiuser_count = $dash->getAttribute((int) $main_response_id, 'multiuser_count');
 	                if ($multiuser_count ?? false)
 	                    $multiuser_count = (int) $multiuser_count + 1;
 	                else
@@ -94,23 +94,23 @@ if ($telegram_user_id = $telegram_response['from']['id'] ?? false) {
 	        	$dash->pushAttribute($response_id, implode('__', $functions->derephrase($last_message_identifier)), $telegram_response['text']);
 
 	        $form_score_name = 'id__'.$tring[1].'__score';
-    		$last_question_correct_response = $dash->getAttribute($response_id , 'last_question_correct_response');
+    		$last_question_correct_response = $dash->getAttribute((int) $response_id , 'last_question_correct_response');
     		if ($last_question_correct_response == $telegram_response['text']) {
-    			$form_score = (int) $dash->getAttribute($response_id , $form_score_name) + 1;
+    			$form_score = (int) $dash->getAttribute((int) $response_id , $form_score_name) + 1;
     			$dash->pushAttribute($response_id, $form_score_name, $form_score);
     		}
 
-    		if (is_int($tring[1]))
-	        	$this_id_type = $dash->getAttribute($tring[1], 'type');
+    		if (is_int($tring[1]) || is_numeric($tring[1]))
+	        	$this_id_type = $dash->getAttribute((int) $tring[1], 'type');
         	
         	if ($this_id_type == 'form') {
 	        	$last_message_identifier_response_type_array = $functions->derephrase($last_message_identifier);
-	        	$last_message_identifier_response_type = $functions->derephrase(json_decode($dash->getAttribute($last_message_identifier_response_type_array[1], 'questions'), true)[($last_message_identifier_response_type_array[2] - 1)])[1][1];
-
+	        	$last_message_identifier_response_type = $functions->derephrase(json_decode($dash->getAttribute((int) $last_message_identifier_response_type_array[1], 'questions'), true)[($last_message_identifier_response_type_array[2] - 1)])[1][1];
+	        	
         		if ($last_message_identifier_response_type == 'mobile' && strlen($telegram_response['text'])!=10) {
 	        		$next_message_identifier = $last_message_identifier;
 	        	}
-	        	else if ($telegram_response['text'] == ($emojis['next'].$emojis['next'].$emojis['next']) && $tring[2]) {
+	        	else if (($telegram_response['text'] == ($emojis['next'].$emojis['next'].$emojis['next'])) && $tring[2]) {
 	        		$next_message_identifier = $last_message_identifier;
 	        	}
 	        	else if (array_values($last_message_response_options)[0] != ($emojis['next'].$emojis['next'].$emojis['next']) && !in_array($telegram_response['text'], array_values($last_message_response_options))) {
