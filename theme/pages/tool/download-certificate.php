4a8c3137bfcb5ef7ddb2b77fe3c69997c8a40348
Apurv Jyotirmay <mail@jyotirmay.dev>
2023-06-08 15:39:39 +0530

    added error message for cert download

--- theme/pages/tool/download-certificate.php
+++ theme/pages/tool/download-certificate.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @var object $dash;
- * @var object $functions;
- */
+* @var object $dash;
+* @var object $functions;
+*/
 // include_once THEME_PATH . '/pages/_header.php';
 include_once TRIBE_ROOT . '_init.php';
 include_once __DIR__ . '/includes/functions.php';
@@ -21,33 +21,38 @@ $i = 0;
 $incomplete = 0;
 
 foreach ($items as $module_id=>$assessment_form_id) {
-    if ($i && $module_id && !isset($response['completed__'.$module_id]))
-      $incomplete = 1;
-    $i++;
+  if ($i && $module_id && !isset($response['completed__'.$module_id]))
+  $incomplete = 1;
+  $i++;
 }
 
-if ($response['id__'.$registration_form_id.'__'.$name_ques_id] && (!$incomplete || ($chatbot['allow_incomplete_certificate_download'] ?? false))) {
-    $form_data = [
-        'name' => $response["id__{$registration_form_id}__{$name_ques_id}"],
-        'Name' => $response["id__{$registration_form_id}__{$name_ques_id}"],
-        'NAME' => $response["id__{$registration_form_id}__{$name_ques_id}"]
-    ];
+if (
+  $response['id__'.$registration_form_id.'__'.$name_ques_id] &&
+  (!$incomplete || ($chatbot['allow_incomplete_certificate_download'] ?? false))
+) {
+  $form_data = [
+    'name' => $response["id__{$registration_form_id}__{$name_ques_id}"],
+    'Name' => $response["id__{$registration_form_id}__{$name_ques_id}"],
+    'NAME' => $response["id__{$registration_form_id}__{$name_ques_id}"]
+  ];
 
-    if (
-      isset($chatbot['certificate_url']) &&
-      trim($chatbot['certificate_url']) !== ''
-    ) {
-      $certificate_template = trim($chatbot['certificate_url']);
-    }
-    else {
-      $certificate_template = THEME_PATH . '/docs/certificate.pdf';
-    }
+  if (
+    isset($chatbot['certificate_url']) &&
+    trim($chatbot['certificate_url']) !== ''
+  ) {
+    $certificate_template = trim($chatbot['certificate_url']);
+  }
+  else {
+    $certificate_template = THEME_PATH . '/docs/certificate.pdf';
+  }
 
-    $certificate_template = urldecode($certificate_template);
-    $certificate_template = str_replace($_ENV['WEB_URL'], TRIBE_ROOT, $certificate_template);
+  $certificate_template = urldecode($certificate_template);
+  $certificate_template = str_replace($_ENV['WEB_URL'], TRIBE_ROOT, $certificate_template);
 
-    $pdf = new Pdf($certificate_template);
-    $pdf->fillForm($form_data)->send("certificate.pdf");
+  $pdf = new Pdf($certificate_template);
+  $pdf->fillForm($form_data)->send("certificate.pdf");
+} else {
+  echo "<strong style='color: red;'><em>Error</em></strong>: Certificate not available, share your id <strong>{$_GET['response_id']}</strong> with person in-charge for help";
 }
 
 die();
