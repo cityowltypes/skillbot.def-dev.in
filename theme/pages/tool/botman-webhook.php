fd0ddab138dce82aecddc2f7ff5c0d63784ff1b7
cityowl / moti <cityowl@hey.com>
2023-07-28 13:23:48 +0530

    webhook

--- theme/pages/tool/botman-webhook.php
+++ theme/pages/tool/botman-webhook.php
@@ -1,3 +1,3 @@
 <?php
-//print_r(exec("curl -X POST -F 'url=https://skillbot.def-dev.in/chatbot/skillbot' https://api.telegram.org/bot2132232112:AAEn6Wles1ClqBwnziZJ6cAVnefA2c5UpVA/setWebhook"));
+//print_r(exec("curl -X POST -F 'url=https://skillbot.def-dev.in/chatbot/digital-didi-phase-2' https://api.telegram.org/bot6049537572:AAF5TqDMPe1Lrpguh-aHHb6KC-WwBWgKaXM/setWebhook"));
 ?>
