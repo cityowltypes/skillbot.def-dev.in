73665f277cfbbbc40616323c10798edd405f3ccd
Apurv Jyotirmay <mail@jyotirmay.dev>
2025-07-16 23:47:52 +0530

    use docker image, and updated ENV values

--- install/setup.sh
+++ install/setup.sh
@@ -3,7 +3,7 @@
 # This scripts creates a new instance for skillbot docker container
 
 random_password() {
-    return $(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
+    echo $(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
 }
 
 random_name() {
@@ -14,44 +14,69 @@ random_name() {
     rand_number=$((RANDOM % 10000))
 
     random_name="${rand_adjective}_${rand_noun}_${rand_number}"
-    return "$random_name"
+    echo "$random_name"
 }
 
-# Generate values
-JUNCTION_PASS=$(random_password)
-
-read -p "Website URL: " WEB_BARE_URL
-read -p "Bot name: " APP_NAME
-read -p "Junction URL: " JUNCTION_URL
-read -p "Enter a port number for Junction: " JUNCTION_PORT
-read -p "Enter a port number for Tribe: " TRIBE_PORT
-
-# remove http(s):// from WEB_BARE_URL
-WEB_BARE_URL=${WEB_BARE_URL#https://}
-WEB_BARE_URL=${WEB_BARE_URL#http://}
-
-APP_UID=$(random_name)
-
-DB_NAME=$APP_UID
-DB_USER="skillbot"
-DB_PASS=$(random_password)
-DB_HOST="${APP_UID}_db"
-
-TRIBE_API_SECRET_KEY=$(random_password)
-
 # start filling the .env file
-[[ ! -f ./.env ]] && cp sample.env .env # create .env file if it doesn't exist already
+if [[ ! -f ./.env ]]; then
+    cp sample.env .env
+    OVERWRITE_ENV="yes"
+else
+    read -p "Do you want to overwrite .env? (yes/no) " OVERWRITE_ENV
+fi
 
-sed -i "s/\$JUNCTION_PASS/$JUNCTION_PASS/g" .env
+if [[ "${OVERWRITE_ENV,,}" == "yes" ]]; then
+    # Generate values
+    JUNCTION_PASS=$(random_password)
 
-sed -i "s/\$WEB_BARE_URL/$WEB_BARE_URL/g" .env
-sed -i "s/\$JUNCTION_URL/$JUNCTION_URL/g" .env
+    read -p "Website URL: " WEB_BARE_URL
+    read -p "Bot name: " APP_NAME
+    read -p "Enter a port number for Junction: " JUNCTION_PORT
+    read -p "Enter a port number for Tribe: " TRIBE_PORT
 
-sed -i "s/\$TRIBE_PORT/$TRIBE_PORT/g" .env
-sed -i "s/\$JUNCTION_PORT/$JUNCTION_PORT/g" .env
+    # remove http(s):// from WEB_BARE_URL
+    WEB_BARE_URL=${WEB_BARE_URL#https://}
+    WEB_BARE_URL=${WEB_BARE_URL#http://}
 
-sed -i "s/\$DB_NAME/$DB_NAME/g" .env
-sed -i "s/\$DB_USER/$DB_USER/g" .env
-sed -i "s/\$DB_PASS/$DB_PASS/g" .env
-sed -i "s/\$DB_HOST/$DB_HOST/g" .env
-sed -i "s/\$TRIBE_API_SECRET_KEY/$TRIBE_API_SECRET_KEY/g" .env
+    APP_UID=$(random_name)
+
+    DB_NAME=$APP_UID
+    DB_USER="skillbot"
+    DB_PASS=$(random_password)
+    DB_HOST="${APP_UID}_db"
+
+    TRIBE_API_SECRET_KEY=$(random_password)
+
+    sed -i "s/\$JUNCTION_PASS/$JUNCTION_PASS/g" .env
+
+    sed -i "s/\$WEB_BARE_URL/$WEB_BARE_URL/g" .env
+
+    sed -i "s/\$TRIBE_PORT/$TRIBE_PORT/g" .env
+    sed -i "s/\$JUNCTION_PORT/$JUNCTION_PORT/g" .env
+
+    sed -i "s/\$DB_NAME/$DB_NAME/g" .env
+    sed -i "s/\$DB_USER/$DB_USER/g" .env
+    sed -i "s/\$DB_PASS/$DB_PASS/g" .env
+    sed -i "s/\$DB_HOST/$DB_HOST/g" .env
+
+    sed -i "s/\$APP_NAME/$APP_NAME/g" .env
+    sed -i "s/\$TRIBE_API_SECRET_KEY/$TRIBE_API_SECRET_KEY/g" .env
+fi
+
+# bash install/prepare.sh
+
+# ENV file is prepared, proceed with import and setups
+if docker compose version >/dev/null 2>&1; then
+    # start docker
+    docker compose up -d
+    # import db
+    docker compose exec db bash /var/www/install/import.sh
+elif command -v docker-compose >/dev/null 2>&1; then
+    # start docker
+    docker-compose up -d
+    # import db
+    docker-compose exec db bash /var/www/install/import.sh
+else
+    echo "Neither 'docker compose' nor 'docker-compose' is available"
+    exit 1
+fi
